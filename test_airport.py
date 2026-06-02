baa8f277c8846ad62c115a36b2dfb57df7827507
pausanchezestrada <pau.sanchez.estrada@estudiantat.upc.edu>
2026-06-02 21:32:20 +0200

    test_airport.py

--- test_airport.py
+++ test_airport.py
@@ -1,17 +1,40 @@
 from airport import *
+from aircraft import *
 
-airport = Airport("LEBL", 41.297445, 2.0832941)
+
+airport = Airport("LEBL", 41.645468, 2.853789)
 SetSchengen(airport)
 PrintAirport(airport)
 
+
 lista_aero = LoadAirports("Airports.txt")
+
 
 AddAirport(lista_aero, airport)
 
-lista_aero = RemoveAirport(lista_aero, "LEBL")
+
+lista_aero = RemoveAirport(lista_aero, "EBBR")
+
 
 SaveSchengenAirports(lista_aero, "SchengenAirports.txt")
 
+
 PlotAirports(lista_aero)
 
+
 MapAirports(lista_aero)
+
+
+lista_aircraft = LoadArrivals("Arrivals.txt")
+print(lista_aircraft)
+PlotArrivals(lista_aircraft)
+SaveFlights(lista_aircraft,"Flights.txt")
+print(lista_aircraft)
+PlotAirlines(lista_aircraft)
+PlotFlightsType(lista_aircraft)
+MapFlights(lista_aircraft)
+print("Se ha generado el mapa de vuelos")
+#función LongDistanceArrivals
+long_distance_flights=LongDistanceArrivals(lista_aircraft)
+print("Número de vuelos de larga distancia:", len(long_distance_flights))
+
